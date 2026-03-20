41ff42d2cdde54523269b8154ca2300197c15823
thedustybox <1124962789@qq.com>
2026-03-20 13:25:27 +0800

    用户模块后端
    1、开发代码生成器
    2、开发用户系统功能
    3、全局权限校验AOP
    4、自定义配置解决精度丢失问题

--- src/main/java/com/hao/haoaicodemother/HaoAiCodeMotherApplication.java
+++ src/main/java/com/hao/haoaicodemother/HaoAiCodeMotherApplication.java
@@ -1,10 +1,12 @@
 package com.hao.haoaicodemother;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
+@MapperScan("com.hao.haoaicodemother.mapper")
 @EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
 public class HaoAiCodeMotherApplication {
---
+++ src/main/java/com/hao/haoaicodemother/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.hao.haoaicodemother.mapper;
+
+import com.mybatisflex.core.BaseMapper;
+import com.hao.haoaicodemother.model.entity.User;
+
+/**
+ * 用户 映射层。
+ *
+ * @author <a href="https://github.com/undejune">undejune</a>
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
